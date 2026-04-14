3947ee33419526a9573ec12dd77ad11b29bcf66f
meongj <meongjiee@gmail.com>
2026-04-14 18:23:29 +0900

    refactor : 중복로직 제거 및 훅으로 통일

--- src/components/CountryCount.tsx
+++ src/components/CountryCount.tsx
@@ -1,16 +1,11 @@
-import {countryQueries} from "@/queries/countries";
+import {useFilteredCountries} from "@/hooks/useFilteredCountries";
 import {CountryFilters} from "@/types/country";
-import {applyFilters} from "@/utils/countryFilters";
-import {useSuspenseQuery} from "@tanstack/react-query";
-import {useMemo} from "react";
 
 interface CountryCountProps {
   filters: CountryFilters;
 }
 
 export function CountryCount({filters}: CountryCountProps) {
-  const {data: countries} = useSuspenseQuery(countryQueries.list());
-  const count = useMemo(() => applyFilters(countries ?? [], filters).length, [countries, filters]);
-
-  return <p className="text-[#6C727F] font-semibold">Found {count} countries</p>;
+  const filtered = useFilteredCountries(filters);
+  return <p className="text-[#6C727F] font-semibold">Found {filtered.length} countries</p>;
 }
--- src/components/CountryTableSection.tsx
+++ src/components/CountryTableSection.tsx
@@ -1,16 +1,12 @@
 import {CountryFilters} from "@/types/country";
 import {CountryTable} from "./CountryTable";
-import {useMemo} from "react";
-import {applyFilters} from "@/utils/countryFilters";
-import {useSuspenseQuery} from "@tanstack/react-query";
-import {countryQueries} from "@/queries/countries";
+import {useFilteredCountries} from "@/hooks/useFilteredCountries";
 
 interface CountryTableSectionProps {
   filters: CountryFilters;
 }
 
 export function CountryTableSection({filters}: CountryTableSectionProps) {
-  const {data: countries} = useSuspenseQuery(countryQueries.list());
-  const filteredCountries = useMemo(() => applyFilters(countries ?? [], filters), [countries, filters]);
-  return <CountryTable countries={filteredCountries} />;
+  const filtered = useFilteredCountries(filters);
+  return <CountryTable countries={filtered} />;
 }
